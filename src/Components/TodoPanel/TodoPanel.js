2f80d60d5aca0e7076711c80a375d6a3b2fde847
Vishal Chandran <codebloodedape@MacBook-Pro.local>
2021-04-17 04:38:36 +0530

    Shortcuts added

--- src/Components/TodoPanel/TodoPanel.js
+++ src/Components/TodoPanel/TodoPanel.js
@@ -11,12 +11,47 @@ class TodoPanel extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            list: this.props.list ?? []
+            list: this.props.list ?? [],
+            addingItem: false
         }
     }
 
     componentDidMount() {
         this.addTodoItemField.focus()
+        this.registerShortcuts()
+    }
+
+    registerShortcuts = () => {
+        window.addEventListener("keydown", event => {
+            // if (event.isComposing || event.keyCode === 229) {
+            //     return;
+            // }
+            switch(event.keyCode) {
+                case 13: 
+                if (this.state.addingItem) {
+                    console.log(event)
+                    event.stopPropagation()
+                    this.state.list.addItem(event.target.value)
+                    this.setState({
+                        list: this.state.list
+                    }, () => {
+                        this.addTodoItemField.value = ''
+                        this.addTodoItemField.focus()
+                    })
+                }
+                break;
+                case 27:
+                    const list = { ...this.state.list }
+                    const title = this.state.list.title
+                    list.updateTitle(title)
+                    this.setState({
+                        list
+                    }, () => {
+                        this.props.closePanel(this.state.list)
+                    })
+            }
+            // do something
+        });
     }
 
     itemToggled = (item) => {
@@ -32,13 +67,6 @@ class TodoPanel extends React.Component {
             list: this.state.list
         })
     }
-
-    // addItem = () => {
-    //     this.state.list.addItem('')
-    //     this.setState({
-    //         list: this.state.list
-    //     })
-    // }
 
     titleFocus = (e) => {
         console.log(e)
@@ -61,7 +89,7 @@ class TodoPanel extends React.Component {
 
     addItemHandler = (e) => {
         // console.log(e.target.nextElementSibling.children[0].innerText)
-        this.state.list.addItem(e.target.nextElementSibling.children[0].innerText)
+        this.state.list.addItem(e.target.nextElementSibling.value)
         this.setState({
             list: this.state.list
         }, () => {
@@ -103,10 +131,16 @@ class TodoPanel extends React.Component {
     addTextFocus = (e) => {
         // console.log(e.target.parentNode.parentNode.classList.add('itemContainerFocused'))
         e.target.parentNode.parentNode.parentNode.classList.add('itemContainerFocused')
+        this.setState({
+            addingItem: true
+        })
     }
 
     addTextBlur = (e) => {
         e.target.parentNode.parentNode.parentNode.classList.remove('itemContainerFocused')
+        this.setState({
+            addingItem: false
+        })
     }
 
     titleChanged = (e) => {
@@ -147,12 +181,18 @@ class TodoPanel extends React.Component {
                 <br />
                 {/* <input type='button' value='Add Item' onClick={this.addItem}></input> */}
                 <div className='itemContainer'>
-                    <div onFocus={this.textAreaFocus} onBlur={this.textAreaBlur} className='item'>
-                    <img onClick={this.addItemHandler} width="30" height="30" className='icon' src={addIcon} />
-                        <p><span onFocus={this.addTextFocus} onBlur={this.addTextBlur} autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" className={classnames('textarea', 'textareaUndone')}
-                            role="textbox" contentEditable ref={(input) => { this.addTodoItemField = input; }} ></span></p>
+                    {/* <div onFocus={this.textAreaFocus} onBlur={this.textAreaBlur} className='item'> */}
+                        <img onClick={this.addItemHandler} width="30" height="30" className='icon' src={addIcon} />
+                        
+                        <input type='text' placeholder='Start typing...' onFocus={this.addTextFocus} onBlur={this.addTextBlur} autocomplete="off" autocorrect="off" 
+                        autocapitalize="off" ref={(input) => { this.addTodoItemField = input; }} spellcheck="false" className='addItemTextField' />
+                        
+                        {/* <p><span onFocus={this.addTextFocus} onBlur={this.addTextBlur} autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" 
+                        className={classnames('textarea', 'textareaUndone')}
+                            role="textbox" contentEditable ref={(input) => { this.addTodoItemField = input; }} ></span></p> */}
+                        
                         <img draggable="false" onClick={this.clearText} width="30" height="30" className='icon' src={removeIcon} />
-                    </div>
+                    {/* </div> */}
                 </div>
                 <div className='itemsContainer'>{items}</div>
                 <br />
